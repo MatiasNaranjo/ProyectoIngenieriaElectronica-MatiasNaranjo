853a061c5aced126bf6003ec6885eacb3e2d338a
Matias Naranjo <mat.naranjo@gmail.com>
2024-10-17 11:24:00 -0300

    Carga de imágenes

---
+++ src/data/data.py
@@ -0,0 +1,90 @@
+import imghdr
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# data_dir = "/data/raw/data"
+data_dir = r"D:\matna\Documents\Escritorio\Facultad\Proyecto\data\raw\data"
+categorias = os.listdir(data_dir)
+
+cant_categorias = len(categorias)
+print("Las " + str(cant_categorias) + " categorias son: " + str(categorias))
+
+# Contar archivos en cada categoría
+for categoria in categorias:
+    categoria_path = os.path.join(data_dir, categoria)
+    if os.path.isdir(categoria_path):
+        num_archivos = len(os.listdir(categoria_path))
+        print(f"- {num_archivos} fotos de {categoria}")
+
+
+# Cargar el dataset de imágenes
+data = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    image_size=(256, 256),
+    batch_size=16,
+    label_mode="int",  # Asegúrate de que las etiquetas sean enteros
+)
+
+data_iterator = data.as_numpy_iterator()
+
+# Get another batch from the iterator
+batch = data_iterator.next()
+
+print()
+print(batch[0].shape)
+print(batch[1])
+
+# Muestro 4 imagenes
+fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+
+
+# Normalizar las imágenes y convertir etiquetas a one-hot encoding
+def process_data(image, label):
+    image = image / 255.0
+    label = tf.one_hot(
+        label, cant_categorias
+    )  # Convertir las etiquetas a one-hot encoding
+    return image, label
+
+
+data = data.map(process_data)
+
+
+scaled_iterator = data.as_numpy_iterator()
+batch = scaled_iterator.next()
+
+fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img)
+
+    # Convertir la etiqueta one-hot a la clase original
+    label_class = tf.argmax(batch[1][idx]).numpy()
+
+    ax[idx].title.set_text(f"Class: {label_class}")
+
+
+# Guardar ----------------------------------------------------------------------
+# Ruta del directorio donde se guardarán las imágenes
+output_dir = r"D:\matna\Documents\Escritorio\Facultad\Proyecto\data\processed"
+import numpy as np
+from PIL import Image  # Asegúrate de importar correctamente PIL
+
+# Guardar cada imagen en su carpeta correspondiente
+for idx, (image, label) in enumerate(data):
+    # Convertir el tensor de la imagen a un formato compatible con PIL
+    image = Image.fromarray(image.numpy().astype(np.uint8))
+
+    # Crear la carpeta para la clase si no existe
+    class_dir = os.path.join(output_dir, f"class_{label.numpy()}")
+    os.makedirs(class_dir, exist_ok=True)
+
+    # Guardar la imagen
+    image.save(os.path.join(class_dir, f"image_{idx}.png"))
+
+print(f"Imágenes guardadas en: {output_dir}")
