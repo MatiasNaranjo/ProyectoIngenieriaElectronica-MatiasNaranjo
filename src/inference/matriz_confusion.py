a2ff12e4080343f73ff1f7bf8b9111969d463a3a
Matias Naranjo <mat.naranjo@gmail.com>
2024-10-31 13:32:54 -0300

    Guardar figura

--- src/inference/matriz_confusion.py
+++ src/inference/matriz_confusion.py
@@ -1,29 +1,28 @@
 import os
-import subprocess
 
 import numpy as np
 import tensorflow as tf
 
 # Directorio base donde guardaremos los conjuntos
-base_save_path = os.path.join("..", "..", "data", "processed")
+base_load_path = os.path.join("..", "..", "data", "processed")
 
-val_save_path = os.path.join(base_save_path, "val")
-test_save_path = os.path.join(base_save_path, "test")
+val_load_path = os.path.join(base_load_path, "val")
+test_load_path = os.path.join(base_load_path, "test")
 
-# Cargar el dataset guardado
-val = tf.data.Dataset.load(val_save_path)
-test = tf.data.Dataset.load(test_save_path)
+# Cargo el dataset guardado
+val = tf.data.Dataset.load(val_load_path)
+test = tf.data.Dataset.load(test_load_path)
 print("Dataset cargado con éxito.")
 
 import glob
 
-# Directorio donde guardas los modelos
+# Directorio donde guardo los modelos
 models_dir = os.path.join("..", "..", "src", "models")
 
-# Buscar todos los archivos de modelo en el directorio que terminan en .keras
+# Busco todos los archivos .keras
 model_files = glob.glob(os.path.join(models_dir, "saved_model_*.keras"))
 
-# Seleccionar el archivo de modelo más reciente
+# Selecciono el archivo de modelo más reciente
 latest_model_file = max(model_files, key=os.path.getctime)
 
 # Cargar el modelo más reciente
@@ -46,11 +45,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import classification_report, confusion_matrix
 
-# Crear matriz de confusión
+# Matriz de confusión
 conf_matrix = confusion_matrix(y_true, y_pred)
 plt.figure(figsize=(10, 8))
 sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues")
 plt.xlabel("Predicción")
 plt.ylabel("Etiqueta Real")
 plt.title("Matriz de Confusión")
-plt.show()
+
+# Guardar ----------------------------------------------------------------
+# Crea la ruta y asegura de que exista
+save_dir = os.path.join("..", "..", "reports", "figures")
+os.makedirs(save_dir, exist_ok=True)  # Crea la carpeta si no existe
+
+# Guardar la figura en la ruta especificada
+save_path = os.path.join(save_dir, "matriz_confusion.jpg")
+plt.savefig(save_path)  # Ajustar dpi si necesitas mayor resolución
+
+print(f"Figura guardada en {save_path}")
