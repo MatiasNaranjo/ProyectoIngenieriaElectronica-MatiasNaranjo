12b32588aa1c1b46c1671c2859025a85a94c8e16
Matias Naranjo <mat.naranjo@gmail.com>
2025-08-07 15:30:41 -0300

    Cambio como cargo los datos para Yolo de forma modular

--- src/data/data_yolo.py
+++ src/data/data_yolo.py
@@ -2,29 +2,20 @@ import os
 
 from roboflow import Roboflow
 
-# Ruta absoluta del proyecto
-abs_path = "D:/matna/Documents/Escritorio/Facultad/Proyecto"
-os.chdir(abs_path)
-print(os.getcwd())
 
-# Definir la ruta donde se descargarán los datos del modelo
-download_dir = os.path.join(abs_path, "data", "yolo")
+def descargar_dataset(abs_path, version=2, api_key="TU_API_KEY", yolo_ver="yolov8"):
+    # Definir la ruta donde se descargarán los datos del modelo
+    download_dir = os.path.join(abs_path, "data", "yolo")
+    os.makedirs(download_dir, exist_ok=True)
+    os.chdir(download_dir)
 
-# Crear la carpeta si no existe
-os.makedirs(download_dir, exist_ok=True)
+    # Conectarse a Roboflow y descargar el dataset
+    rf = Roboflow(api_key=api_key)
+    project = rf.workspace("proyecto-final-labels").project(
+        "proyecto_final_electronica"
+    )
+    dataset = project.version(version).download(yolo_ver)
 
-# Cambiar el directorio actual
-os.chdir(download_dir)
-print(download_dir)
-
-# Número de versión del dataset en Roboflow
-ver = 2
-# Autenticación en Roboflow con una clave API
-rf = Roboflow(api_key="1BqdoBrABHEibVODyrPg")
-# Cargar el proyecto desde Roboflow Workspace
-project = rf.workspace("proyecto-final-labels").project("yolo-uifhe")
-version = project.version(ver)
-
-dataset = version.download("yolov8")
-
-os.chdir(abs_path)
+    # Volver al directorio principal
+    os.chdir(abs_path)
+    return dataset
