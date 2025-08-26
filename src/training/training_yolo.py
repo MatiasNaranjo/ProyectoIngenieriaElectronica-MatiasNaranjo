b86e12335ccc3519eb5d5e1d873d778ec5ddb0dd
Matias Naranjo <mat.naranjo@gmail.com>
2025-08-26 15:28:03 -0300

    Modularizo el entrenemiento de YOLO; agrego modelo y cache a gitignore

--- src/training/training_yolo.py
+++ src/training/training_yolo.py
@@ -2,30 +2,34 @@ import torch
 import torchvision
 from ultralytics import YOLO
 
-# Verifico si PyTorch, CUDA y torchvision están instalados correctamente
-print(torch.__version__)
-print(torch.cuda.is_available())
-print(torch.version.cuda)
-print(torch.cuda.get_device_name(0))
-print(torchvision.__version__)
 
-# Cargo el modelo YOLO preentrenado
-model = YOLO("yolov8n.pt")
-ver = 2
+def entrenar_yolo(abs_path, version):
+    # Verifico si PyTorch, CUDA y torchvision están instalados correctamente
+    print(torch.__version__)
+    print(torch.cuda.is_available())
+    print(torch.version.cuda)
+    print(torch.cuda.get_device_name(0))
+    print(torchvision.__version__)
 
-yaml_path = "D:/matna/Documents/Escritorio/Facultad/Proyecto/data/yolo/yolo-2/data.yaml"
+    # Cargo el modelo YOLO preentrenado
+    model = YOLO("yolov8n.pt")
+    yaml_path = (
+        abs_path
+        + "/data/yolo/Proyecto_final_electronica-"
+        + str(version)
+        + "/data.yaml"
+    )
 
-
-# Configuro y comienzo el entrenamiento del modelo YOLO
-model.train(
-    data=yaml_path,
-    imgsz=640,
-    batch=2,
-    epochs=400,
-    workers=0,
-    device=0,
-    verbose=True,
-    amp=False,  # Desactiva AMP explícitamente
-    # project="resultados_yolo",  # Carpeta donde se guardará el modelo
-    # name="experimento_1"  # Nombre específico de la subcarpeta
-)
+    # Configuro y comienzo el entrenamiento del modelo YOLO
+    model.train(
+        data=yaml_path,
+        imgsz=1080,
+        batch=2,
+        epochs=4,
+        workers=0,
+        device=0,
+        verbose=True,
+        amp=False,  # Desactiva AMP explícitamente
+        # project="resultados_yolo",  # Carpeta donde se guardará el modelo
+    )
+    return model
