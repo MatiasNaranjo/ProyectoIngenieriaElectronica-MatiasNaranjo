29101fc6cc31e9d3f11d1829f2415b990d216a1a
LEGION5MATIAS\matna <mat.naranjo@gmail.com>
2026-01-19 17:20:54 -0300

    feat(config, deploy): ampliar deploy para copiar config, types y utils usando nuevos campos

--- src/deploy.py
+++ src/deploy.py
@@ -1,6 +1,7 @@
 from src.utils.files import (
     exportar_a_raspberry,
     importar_de_raspberry,
+    list_files_by_prefix,
 )
 
 
@@ -13,6 +14,41 @@ def deploy_raspi(config):
         raspberry_ip=config.raspi.ip,
         files=config.pc.files_up,
         folders=config.pc.folders_up,
+        key_path=config.pc.key_path,
+        passphrase=config.pc.passphrase,
+    )
+
+    # Copiar archivos de configuración PC a Raspberry PI
+    list_config = list_files_by_prefix(config.pc.dir_config, config.pc.prefix_config)
+    exportar_a_raspberry(
+        local_dir=config.pc.dir_config,
+        remote_dir=config.raspi.dir_config,
+        raspberry_user=config.raspi.user,
+        raspberry_ip=config.raspi.ip,
+        files=list_config,
+        key_path=config.pc.key_path,
+        passphrase=config.pc.passphrase,
+    )
+
+    # Copiar archivos de types PC a Raspberry PI
+    list_types = list_files_by_prefix(config.pc.dir_types, config.pc.prefix_types)
+    exportar_a_raspberry(
+        local_dir=config.pc.dir_types,
+        remote_dir=config.raspi.dir_types,
+        raspberry_user=config.raspi.user,
+        raspberry_ip=config.raspi.ip,
+        files=list_types,
+        key_path=config.pc.key_path,
+        passphrase=config.pc.passphrase,
+    )
+
+    # Copiar config_loader PC a Raspberry PI
+    exportar_a_raspberry(
+        local_dir=config.pc.dir_utils,
+        remote_dir=config.raspi.dir_utils,
+        raspberry_user=config.raspi.user,
+        raspberry_ip=config.raspi.ip,
+        files=config.pc.file_config_loader,
         key_path=config.pc.key_path,
         passphrase=config.pc.passphrase,
     )
--- src/utils/config/types/config_types_pc_update.py
+++ src/utils/config/types/config_types_pc_update.py
@@ -2,18 +2,29 @@ from pydantic import BaseModel, Field
 
 
 class RaspberryConfig(BaseModel):
-    dir: str
+    dir_base: str
+    dir_config: str | None = None
+    dir_utils: str | None = None
+    dir_utils_config: str | None = None
+    dir_types: str | None = None
     user: str
     ip: str
     folders_down: list[str] = Field(default_factory=list)
 
 
 class PcConfig(BaseModel):
-    dir: str
+    dir_base: str
+    dir_raspi: str | None = None
+    dir_config: str | None = None
+    dir_types: str | None = None
+    dir_utils: str | None = None
+    file_config_loader: list[str] = Field(default_factory=list)
     key_path: str | None = None
     passphrase: str | None = None
     files_up: list[str] = Field(default_factory=list)
     folders_up: list[str] = Field(default_factory=list)
+    prefix_config: str | None = None
+    prefix_types: str | None = None
 
 
 class AppConfig(BaseModel):
