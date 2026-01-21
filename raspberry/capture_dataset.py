eb881c71a9ef114edc2340dbfe1ffded97a6c03e
LEGION5MATIAS\matna <mat.naranjo@gmail.com>
2026-01-21 14:35:35 -0300

    refactor(camera): cargar configuración de cámara y rutas desde archivo config

--- raspberry/capture_dataset.py
+++ raspberry/capture_dataset.py
@@ -1,11 +1,23 @@
-from src.utils.camera_utils import capture_photos, init_camera
+from src.camera.camera_utils import capture_photos, init_camera
 
-# Inicializar la cámara
-picam2 = init_camera(resolution=1440)
+from src.utils.config_loader import ConfigLoader
 
-# Directorio donde guardar las fotos
-clase = "Te"
-output_dir = "output/data/" + clase
 
-# Capturar fotos
-capture_photos(picam2, output_dir, n_photos=40, delay=0.5)
+def main():
+    # Cargar la configuración
+    config = ConfigLoader(func_name="capture").load()
+
+    # Inicializar la cámara
+    picam2 = init_camera(resolution=config.cam.resolution)
+
+    # Directorio donde guardar las fotos
+    output_dir = config.cam.output_dir + "/" + config.cam.clase
+
+    # Capturar fotos
+    capture_photos(
+        picam2, output_dir, n_photos=config.cam.n_photos, delay=config.cam.delay
+    )
+
+
+if __name__ == "__main__":
+    main()
