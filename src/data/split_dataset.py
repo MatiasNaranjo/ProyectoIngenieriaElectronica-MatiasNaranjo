8c63f1b56634197056efbf54a930b2261f1a6600
LEGION5MATIAS\matna <mat.naranjo@gmail.com>
2026-02-17 15:43:28 -0300

    feat(dataset): implementar creación de carpetas para splits en la función split_by_session

--- src/data/split_dataset.py
+++ src/data/split_dataset.py
@@ -8,6 +8,13 @@ def validate_split_ratio(split_ratio: dict[str, float]) -> None:
         raise ValueError("Los porcentajes deben sumar 1.0")
 
 
+def create_split_folders(dst_path: Path, split_ratio: dict[str, float]) -> None:
+    # Crear carpetas para cada split (train, val y/o test) dentro de dst_path/split/
+    for split in split_ratio.keys():
+        (dst_path / "split" / split / "images").mkdir(parents=True, exist_ok=True)
+        (dst_path / "split" / split / "labels").mkdir(parents=True, exist_ok=True)
+
+
 def split_by_session(
     input_dataset_path: Path,
     output_dataset_path: Path,
@@ -15,3 +22,6 @@ def split_by_session(
 ) -> None:
     # Validar el split_ratio
     validate_split_ratio(split_ratio)
+
+    # Crear carpetas para los splits
+    create_split_folders(output_dataset_path, split_ratio)
