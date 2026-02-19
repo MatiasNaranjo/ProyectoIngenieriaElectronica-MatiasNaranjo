2d0f3f1efabff52a454927ad159c697bd8ac0133
LEGION5MATIAS\matna <mat.naranjo@gmail.com>
2026-02-19 17:02:38 -0300

    feat(dataset): integrar etapa de split con configuración desde dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,5 +1,6 @@
 from src.data.data_yolo import descargar_dataset
 from src.data.merge_dataset import merge_roboflow_dataset
+from src.data.split_dataset import split_by_session
 from src.utils.config_loader import ConfigLoader
 
 
@@ -22,6 +23,14 @@ def main():
         merge_roboflow_dataset(
             input_dir=config.paths.data_yolo_raw,
             output_dir=config.paths.data_yolo,
+        )
+
+    if config.pipeline.split:
+        split_by_session(
+            input_dataset_path=config.paths.data_yolo_merged,
+            output_dataset_path=config.paths.data_yolo,
+            split_ratio=config.dataset.split_cfg.model_dump(),
+            seed=config.dataset.seed,
         )
 
 
--- src/utils/config/types/config_types_pc_dataset.py
+++ src/utils/config/types/config_types_pc_dataset.py
@@ -15,6 +15,17 @@ class RoboflowConfig(BaseModel):
     yolo_ver: str | None = None
 
 
+class DatasetSplitConfig(BaseModel):
+    train: float = 0.7
+    val: float = 0.2
+    test: float = 0.1
+
+
+class DatasetConfig(BaseModel):
+    split_cfg: DatasetSplitConfig
+    seed: int = 42
+
+
 class PathsConfig(BaseModel):
     data_yolo: str | None = None
     data_yolo_raw: str | None = None
@@ -24,4 +35,5 @@ class PathsConfig(BaseModel):
 class AppConfig(BaseModel):
     pipeline: PipelineConfig
     roboflow: RoboflowConfig
+    dataset: DatasetConfig
     paths: PathsConfig
